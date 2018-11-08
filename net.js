83405f041721c5c6bc6208200a5f61f35498d96a
cazuba <krlos2290@gmail.com>
2018-11-07 22:31:32 -0600

    Finish chat node 'terminal' application

--- net.js
+++ net.js
@@ -2,17 +2,35 @@ process.stdout.write('\u001B[2J\u001B[0;0f');
 
 const server = require('net').createServer();
 
+let counter = 0;
+let sockets = {};
+
+function timestamp() {
+    const now = new Date();
+    return `${now.getHours()}:${now.getMinutes()}`;
+}
+
 server.on('connection', socket => {
     console.log('Client connected');
-    socket.write('Welcome new client!\n');
+    socket.id = counter++;
+    socket.write('Welcome!\nPlease type your name:');
 
     socket.on('data', (data) => {
-        console.log('Data is ', data);
-        socket.write('Data is:');
-        socket.write(data);
+        if (!sockets[socket.id]) {
+            socket.nickname = data.toString().trim();
+            socket.write(`Welcome ${socket.nickname}!\n`);
+            sockets[socket.id] = socket;
+            return;
+        }
+        Object.entries(sockets).forEach(([key, cs]) => {
+            if (socket.id == key) return;
+            cs.write(`${socket.nickname} ${timestamp()}: `);
+            cs.write(data);
+        });
     });
 
     socket.on('end', () => {
+        delete sockets[socket.id];
         console.log('Client desconnected');
     });
 
